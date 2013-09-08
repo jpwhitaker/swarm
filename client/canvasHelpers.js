3a9916c625b03b3c7bdb8d8d7c2fc582d8a6feee
JP Whitaker <jpw@hawaii.edu>
2013-09-07 22:44:49 -0700

    dragging hexagons around the board.

--- client/canvasHelpers.js
+++ client/canvasHelpers.js
@@ -12,6 +12,100 @@ Swarm.resizeCanvas = function(){
     var windowHeight = window.innerHeight;
     ctx.canvas.width = windowWidth;
     ctx.canvas.height = windowHeight;
+    for (hex in hexagons){
+      hexagons[hex].draw()
+    }
   }
+}
+
+
+
+Swarm.dragHex = function(){
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+  var selection = null;
+  var dragging = false;
+
+  canvas.addEventListener('mousedown', function(e){
+    hexagons.forEach(function(hexagon){
+      if(hexagon.isInBounds(e.x,e.y) && dragging == false){
+        dragging = true;
+        selection = hexagon;
+        console.log(selection)
+      } else{
+        dragging = false;
+        selection = null;
+      }
+    })
+  })
+
+  canvas.addEventListener('mousemove', function(e){
+    if(selection != null){
+      console.log(e)
+      selection.Points.forEach(function(point){
+        console.log(e.x)
+        point.X += e.webkitMovementX;
+        point.Y += e.webkitMovementY;
+      })
+      Swarm.ctx.clearRect(0,0, Swarm.ctx.canvas.clientWidth, Swarm.ctx.canvas.clientHeight)
+      hexagons.forEach(function(hexagon){
+        hexagon.draw()
+      })
+    }
+  })
 
 }
+
+// Swarm.canvasState = function(){
+//   var canvas = document.getElementById('canvas');
+//   var ctx = canvas.getContext('2d');
+
+//   this.valid = false;
+//   this.shapes = [];
+//   this.dragging = false;
+//   this.selection = null;
+//   this.dragoffx = 0;
+//   this.dragoffy = 0;
+
+//   var myState = this;
+
+//   //prevent double click selecting text
+//   canvas.addEventListener('selectstart', function(e) {e.preventDefault(); return false;}, false);
+
+//   //Up, down, and move are for dragging
+//   canvas.addEventListener('mousedown', function(e) {
+//     var mx = e.x;
+//     var my = e.y;
+//     for (hex in hexagons){
+//       if (hexagons[hex].isInBounds(e.x, e.y)){
+//         var mySel = hexagons[hex];
+//         myState.dragoffx = mx - mySel.x;
+//         myState.dragoffy = my - mySel.y;
+//         myState.dragging = true;
+//         myState.selection = mySel;
+//         myState.valid = false;
+//         return;
+//       }
+//     }
+
+//   if (myState.selection) {
+//     myState.selection = null;
+//     myState.valid = false; // Need to clear the old selection border
+//   }
+
+//   canvas.addEventListener('mousemove', function(e){
+//     if(myState.dragging){
+//       if(myState.selection !== null){
+//         console.log('should be moving')
+//         myState.selection.Points.forEach(function (point){
+          
+//           point.X = e.x;
+//           point.Y = e.y;
+//         })
+//       myState.selection.draw()
+//       }
+//     }
+//   })
+
+//   })
+// }
--- swarm.js
+++ swarm.js
@@ -7,21 +7,28 @@ if (Meteor.isClient) {
       // x = new Swarm.Hexagon(100, 100, 1);
       // x.draw()
 
-      canvas.addEventListener('click', function(event){
-        for (hex in hexagons){
-          if (hexagons[hex].isInBounds(event.x, event.y)){
-            console.log(+hex + 1)
-          }
-        }
-        })
+      // canvas.addEventListener('click', function(event){
+      //   for (hex in hexagons){
+      //     if (hexagons[hex].isInBounds(event.x, event.y)){
+      //       console.log(+hex + 1)
+      //     }
+      //   }
+      //   })
 
       canvas.addEventListener('dblclick', function(event){
         hex = new Swarm.Hexagon(hexagons.length+1, event.x, event.y)
         hexagons.push(hex)
+        Swarm.ctx.clearRect(0,0, Swarm.ctx.canvas.clientWidth, Swarm.ctx.canvas.clientHeight)
         for (hex in hexagons){
           hexagons[hex].draw()
         }
       })
+
+      canvas.addEventListener('drag', function(event){
+        console.log(event)
+      })
+
+      Swarm.dragHex();
 
   })
 }
